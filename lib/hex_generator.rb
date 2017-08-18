f383a60c38087f805641fd18e695918013c2b933
highfiverobot <tim.a.james@gmail.com>
2017-08-18 02:40:17 -0400

    stupid code, complete this later.π

--- lib/hex_generator.rb
+++ lib/hex_generator.rb
@@ -1,12 +1,14 @@
 class HexGenerator
-  # def 'get_letter' do
-  #   'A'
-  #   end
-  # def 'get_number' do
-  #   '1'
-  # end
-  # def 'get_full_hex' do
-  #   'A3'
-  # end
+  def get_letter
+    ('A'..'F').to_a.sample
+  end
+
+  def get_number
+    (0..9).to_a.sample
+  end
+
+  def get_full_hex
+    "#{get_letter}#{get_number}"
+  end
 
 end
